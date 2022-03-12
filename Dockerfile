2dc7bd0c0a640c6afab6e0d63d78e2084cb414cb
N4NO <nathan.rouse56734@gmail.com>
2022-03-12 00:24:43 +0000

    added logging and handlers

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,12 @@ WORKDIR /usr/src/app
 COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
 RUN npm install --production --silent && mv node_modules ../
 COPY . .
-EXPOSE 3001
+EXPOSE 80
 RUN chown -R node /usr/src/app
 USER node
-CMD ["node", "server.js"]
+CMD ["node server.js"]
 
+ENV PORT=80
 
 #confirm Installs
 RUN node -v
--- server.js
+++ server.js
@@ -1,11 +1,31 @@
-'use strict';
+'use strict'
+require('dotenv').config()
+
 const express = require('express')
 const api = express()
+const port = process.env.PORT || 80
+
+console.log("website server started on port:", port)
+
+//app.use
+api.use("/css", express.static(__dirname + "/css"))
+
+api.use(function (req,res,next) {
+    console.log("Accessed ip " + req.ip, req.hostname + req.url)
+    next()
+})
+
+/*
+ * GET handlers
+ */
 
 api.get('/', function (req, res) {
-    console.log("accessed")
     res.sendFile(__dirname + "/sign-in.html")
 })
+
+/*
+ * POST handlers
+ */
 
 api.post('/signin', function (req, res) {
     res.json({state: "accepted"})
@@ -13,4 +33,4 @@ api.post('/signin', function (req, res) {
 
 
 
-api.listen(3001)
+api.listen(port)
