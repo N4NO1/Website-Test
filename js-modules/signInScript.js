c86271dc9e379781e46b3077e42278e2c3622bb6
N4NO1 <nathan.rouse56734@gmail.com>
2022-03-17 21:13:04 +0000

    added database query function, but it doesn't allow requirement to be set in signInScript.js => investigate

--- js-modules/signInScript.js
+++ js-modules/signInScript.js
@@ -1,7 +1,11 @@
 const md5 = require('md5')
+
+
 module.exports = handleCredentials
 
-const { RDSClient, AddRoleToDBClusterCommand } = require("@aws-sdk/client-rds");
+
+
+
 
 function handleCredentials(username, password) {
 
