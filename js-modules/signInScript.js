57d74e45ed3d96eaba20ca10493bbba4673af94e
N4NO1 <nathan.rouse56734@gmail.com>
2022-03-17 20:29:43 +0000

    aws-sdk added

--- js-modules/signInScript.js
+++ js-modules/signInScript.js
@@ -1,6 +1,8 @@
 const md5 = require('md5')
 module.exports = handleCredentials
 
+const { RDSClient, AddRoleToDBClusterCommand } = require("@aws-sdk/client-rds");
+
 function handleCredentials(username, password) {
 
         const credMatch = { result: "test" }
