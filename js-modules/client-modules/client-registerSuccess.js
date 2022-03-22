a3ad239a8f53c2c6f1b64d0d126358dcdfcec50d
N4NO <nathan.rouse56734@gmail.com>
2022-03-22 22:47:14 +0000

    query lookbacks enabled

--- js-modules/client-modules/client-registerSuccess.js
+++ js-modules/client-modules/client-registerSuccess.js
@@ -1,12 +1,13 @@
-document.onload = () => {
+const messageBox = document.getElementById('message');
+const link = document.getElementById('link');
 
-    const messageBox = document.getElementById('message');
+function loaded() {
 
-    const params = new Proxy(new URLSearchParams(window.location.search), {
-        get: (searchParams, prop) => searchParams.get(prop),
-      });
-      console.log(params.state)
+    
+    console.log("test")
+    const params = new URLSearchParams(window.location.search)
 
-    messageBox.innerHTML = ` ${params.state} successfully registered with email ${params.email} `;
+    document.getElementById('link').href = `/sign-in?email=${params.get("email")}`
+    messageBox.innerHTML = `${params.get("state")} successfully registered with email ${params.get("email")} `;
 }
 //https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
